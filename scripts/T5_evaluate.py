0bdb269ec20365bc0a87cdfac34002aa7ef99cbf
Hisarlik <mentared@gmail.com>
2022-04-14 13:34:55 +0200

    remove dataset. gitignore changed

--- scripts/T5_evaluate.py
+++ scripts/T5_evaluate.py
@@ -18,8 +18,8 @@ if __name__ == "__main__":
         WordLengthRatio=dict(target_ratio=0.75),
         CharLengthRatio=dict(target_ratio=0.65),
         LevenshteinRatio=dict(target_ratio=0.5),
-        DependencyTreeDepthRatio=dict(target_ratio=1.4),
-        WordRankRatio=dict(target_ratio=1.15)
+        DependencyTreeDepthRatio=dict(target_ratio=0.8),
+        WordRankRatio=dict(target_ratio=0.9)
     )
 
     experiment_id = None
--- scripts/optimization.py
+++ scripts/optimization.py
@@ -26,11 +26,11 @@ def objective(trial: optuna.trial.Trial, experiment_id, dataset) -> float:
 
 
     features = dict(
-            WordLengthRatio=dict(target_ratio=trial.suggest_float('WordRatio', 0.20, 1.5, step=0.05)),
-            CharLengthRatio=dict(target_ratio=trial.suggest_float('CharRatio', 0.20, 1.5, step=0.05)),
-            LevenshteinRatio=dict(target_ratio=trial.suggest_float('LevenshteinRatio', 0.20, 1.5, step=0.05)),
-            DependencyTreeDepthRatio=dict(target_ratio=trial.suggest_float('DepthTreeRatio', 0.20, 1.5, step=0.05)),
-            WordRankRatio=dict(target_ratio=trial.suggest_float('WordRankRatio', 0.20, 1.5, step=0.05)))
+            WordLengthRatio=dict(target_ratio=trial.suggest_float('WordRatio', 0.40, 1.5, step=0.05)),
+            CharLengthRatio=dict(target_ratio=trial.suggest_float('CharRatio', 0.40, 1.5, step=0.05)),
+            LevenshteinRatio=dict(target_ratio=trial.suggest_float('LevenshteinRatio', 0.40, 1.5, step=0.05)),
+            DependencyTreeDepthRatio=dict(target_ratio=trial.suggest_float('DepthTreeRatio', 0.40, 1.5, step=0.05)),
+            WordRankRatio=dict(target_ratio=trial.suggest_float('WordRankRatio', 0.40, 1.5, step=0.05)))
 
     experiment_id = None
 
@@ -41,14 +41,14 @@ def objective(trial: optuna.trial.Trial, experiment_id, dataset) -> float:
 
 if __name__ == '__main__':
 
-    expe_id = "20220404092551"
+    expe_id = "20220413100757"
     dataset = SIMPLETEXT_DATASET
     trials = 500
 
     # Wrap the objective inside a lambda and call objective inside it
     func = lambda trial: objective(trial, expe_id, dataset)
     study = optuna.create_study(study_name='Tokens_study', direction="maximize")
-    study.optimize(func, n_trials=500)
+    study.optimize(func, n_trials=trials)
 
     logger.info("Number of finished trials: {}".format(len(study.trials)))
 
