35504a853b2b6c276284f38ba679bad265e994ac
Hisarlik <mentared@gmail.com>
2022-04-18 09:30:20 +0200

    get_metrics method modified to store predictions at the beginning.

--- scripts/T5_evaluate.py
+++ scripts/T5_evaluate.py
@@ -15,11 +15,11 @@ from source.evaluation import evaluate
 
 if __name__ == "__main__":
     features = dict(
-        WordLengthRatio=dict(target_ratio=0.7000000000000001),
-        CharLengthRatio=dict(target_ratio=0.7000000000000001),
-        LevenshteinRatio=dict(target_ratio=0.6000000000000001),
-        DependencyTreeDepthRatio=dict(target_ratio=0.9500000000000001),
-        WordRankRatio=dict(target_ratio=0.4)
+        WordLengthRatio=dict(target_ratio=0.7),
+        CharLengthRatio=dict(target_ratio=0.6),
+        LevenshteinRatio=dict(target_ratio=0.65),
+        DependencyTreeDepthRatio=dict(target_ratio=0.75),
+        WordRankRatio=dict(target_ratio=0.65)
     )
 
     experiment_id = None
